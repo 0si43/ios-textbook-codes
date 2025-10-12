a038a44998a58cb3feb38662228a75a48f7dfaeb
0si43 <45909001+0si43@users.noreply.github.com>
2025-10-12 12:49:19 +0900

    add error handling

--- Example/SwiftUI_GitHubUserList/GitHubUserList/GitHubAPIClient.swift
+++ Example/SwiftUI_GitHubUserList/GitHubUserList/GitHubAPIClient.swift
@@ -14,9 +14,18 @@ final class GitHubAPIClient {
         guard let url = URL(string: "\(baseURL)/users") else { fatalError("confirm usersURL") }
         let request = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: request)
-        // FIXME: - responseに対するエラーハンドリング
+        guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+            throw HTTPResponseError()
+        }
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         return try decoder.decode([User].self, from: data)
+    }
+}
+
+struct HTTPResponseError: LocalizedError {
+    var errorDescription: String? {
+        "HTTPレスポンスが正常ではありません"
     }
 }
